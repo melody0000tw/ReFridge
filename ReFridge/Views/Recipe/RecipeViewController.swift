9e9407228ac89e8bd2daf89d08ab9bc346ed4322
Yun Chieh Lee <melody0000tw@gmail.com>
2024-04-16 11:07:32 +0800

    filter recipe fitness percentage >= 0.5

--- ReFridge/Views/Recipe/RecipeViewController.swift
+++ ReFridge/Views/Recipe/RecipeViewController.swift
@@ -19,6 +19,9 @@ class RecipeViewController: UIViewController {
         }
     }
     
+    var filterdRecipes: [Recipe] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
@@ -41,6 +44,10 @@ class RecipeViewController: UIViewController {
                 case .success(let recipes):
                     print("got recipes! \(recipes)")
                     self.recipes = recipes
+                    filterRecipes(recipes: self.recipes) { filterdRecipes in
+                        self.filterdRecipes = filterdRecipes
+                        print("filterRecipe fetch 成功: \(self.filterdRecipes)")
+                    }
                 case .failure(let error):
                     print("error: \(error)")
                 }
@@ -62,6 +69,80 @@ class RecipeViewController: UIViewController {
             })
         }
         return queryFoodType
+    }
+    
+    
+    private func filterRecipes(recipes: [Recipe], completion: @escaping ([Recipe]) -> Void) {
+        var filterdRecipes = [Recipe]()
+        let dispatchGroup = DispatchGroup()
+        for recipe in recipes {
+            dispatchGroup.enter()
+            print("Recipe id: \(recipe.recipeId) 小組任務開始")
+            checkFitness(for: recipe) { percentage in
+                if percentage >= 0.5 {
+                    filterdRecipes.append(recipe)
+                }
+                dispatchGroup.leave()
+                print("Recipe id: \(recipe.recipeId) 小組任務結束")
+            }
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            print("全部小組任務結束，回傳 filterd Recipe")
+            completion(filterdRecipes)
+        }
+    }
+    
+    
+    
+    private func checkFitness(for recipe: Recipe, completion: @escaping (Double) -> Void) {
+        var allTypes = [FoodType]()
+        var lackTypes = [FoodType]()
+        var checkTypes = [FoodType]()
+        
+        let dispatchGroup = DispatchGroup() // 建立小組任務
+        for ingredient in recipe.ingredients {
+            dispatchGroup.enter() // 小組任務開始
+            let typeId = ingredient.typeId
+            
+            // 找小卡有沒有，歸類到 lack or check
+            Task {
+                await firestoreManager.queryFoodCard(by: typeId, completion: { result in
+                    switch result {
+                    case .success(let foodCards):
+                        if foodCards.count == 0 {
+                            // 缺乏的食材，先用id找到type
+                            let foodType = FoodTypeData.share.queryFoodType(typeId: typeId)
+                            guard let foodType = foodType else {
+                                print("找不到 foodtype")
+                                return
+                            }
+                            print("把\(foodType.typeName)放到 lack")
+                            lackTypes.append(foodType)
+                            allTypes.append(foodType)
+                        } else {
+                            // 冰箱有的，用id找到type
+                            let foodType = FoodTypeData.share.queryFoodType(typeId: typeId)
+                            guard let foodType = foodType else {
+                                print("找不到 foodtype")
+                                return
+                            }
+                            print("把\(foodType.typeName)放到 check")
+                            checkTypes.append(foodType)
+                            allTypes.append(foodType)
+                        }
+                    case .failure(let error):
+                        print("error: \(error)")
+                    }
+                })
+                dispatchGroup.leave()
+            }
+        }
+        dispatchGroup.notify(queue: .main) {
+            let fitPercentage = Double(checkTypes.count) / Double(allTypes.count)
+            print("Recipe id: \(recipe.recipeId) 比對冰箱吻合度: \(fitPercentage.rounding(toDecimal: 3))")
+            completion(fitPercentage.rounding(toDecimal: 3))
+        }
     }
 }
 
