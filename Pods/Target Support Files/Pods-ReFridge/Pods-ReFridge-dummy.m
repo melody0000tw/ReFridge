0e42b4d3a4c4b0180a3563241d5edb4190804302
melody0000tw <141668860+melody0000tw@users.noreply.github.com>
2024-04-17 01:08:28 +0800

    Revert "Sprint 1 "

--- Pods/Target Support Files/Pods-ReFridge/Pods-ReFridge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReFridge : NSObject
-@end
-@implementation PodsDummy_Pods_ReFridge
-@end
---
+++ ReFridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReFridge
+//
+//  Created by Melody Lee on 2024/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
