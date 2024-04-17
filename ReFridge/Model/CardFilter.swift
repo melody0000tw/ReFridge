4a2c57fed648b2ac75fe6a350c9361397118d6ec
Yun Chieh Lee <melody0000tw@gmail.com>
2024-04-17 12:40:51 +0800

    feature: food card filter menu

---
+++ ReFridge/Model/CardFilter.swift
@@ -0,0 +1,19 @@
+//
+//  CardFilter.swift
+//  ReFridge
+//
+//  Created by Melody Lee on 2024/4/17.
+//
+
+import Foundation
+
+struct CardFilter {
+    var categoryId: Int?
+    var sortBy: CardSortMethod
+}
+
+enum CardSortMethod {
+    case remainingDay
+    case createDay
+    case category
+}
