82244f1aa344c5f8d45afb843337682f3050c2f1
Yun Chieh Lee <melody0000tw@gmail.com>
2024-05-14 15:57:26 +0800

    create fake datas

--- ReFridge/Managers/TextScanManager.swift
+++ ReFridge/Managers/TextScanManager.swift
@@ -30,18 +30,24 @@ class TextScanManager {
             try handler.perform(requests)
         } catch {
             print("Failed to perform request:", error)
-            completion(nil)
+            let scanResult = self.createFakedatas()
+            completion(scanResult)
+//            completion(nil)
         }
     }
     
     private func handleTextRecognitionResults(request: VNRequest, error: Error?, completion: @escaping (ScanResult?) -> Void) {
         if let error = error {
             print("Text detection error: \(error)")
-            completion(nil)
+            let scanResult = self.createFakedatas()
+            completion(scanResult)
+//            completion(nil)
             return
         }
         guard let observations = request.results as? [VNRecognizedTextObservation], !observations.isEmpty else {
-            completion(nil)
+            let scanResult = self.createFakedatas()
+            completion(scanResult)
+//            completion(nil)
             return
         }
 
@@ -52,19 +58,35 @@ class TextScanManager {
     private func processDetectedText(_ detectedText: [String], completion: @escaping (ScanResult?) -> Void) {
         openAIManager.filterArrays(inputArray: detectedText) { result in
             switch result {
-            case .success(let foodReply) where foodReply.food.isEmpty:
-                print("Food array is empty, retrying...")
-                self.retryFiltering(detectedText, completion: completion)
+//            case .success(let foodReply) where foodReply.food.isEmpty:
+//                print("Food array is empty, retrying...")
+//                self.retryFiltering(detectedText, completion: completion)
             case .success(let foodReply):
-                let scanResult = self.createScanResult(foodReply: foodReply)
-                completion(scanResult)
+                if foodReply.food.count < 5 || foodReply.notFood.count < 5 {
+                    print("less than 5")
+                    let scanResult = self.createFakedatas()
+                    completion(scanResult)
+                } else {
+                    print("more than 5")
+                    let scanResult = self.createScanResult(foodReply: foodReply)
+                    completion(scanResult)
+                }
             case .failure(let error):
                 print(error.localizedDescription)
-                let foodReply = AIFoodReplay(food: [], notFood: detectedText)
-                let scanResult = self.createScanResult(foodReply: foodReply)
+                print("error")
+//                let foodReply = AIFoodReplay(food: [], notFood: detectedText)
+//                let scanResult = self.createScanResult(foodReply: foodReply)
+                let scanResult = self.createFakedatas()
                 completion(scanResult)
             }
         }
+    }
+    
+    // create fake datas
+    private func createFakedatas() -> ScanResult {
+        let newFoodReply = AIFoodReplay(food: ["三重巧克力餅乾*", "半甜巧克力餅乾*", "抹茶白巧克力餅乾1", "棉花糖奶油夾心餅乾", "白巧克力夏威夷果", "經典布朗尼奇樂*", "冰美式咖啡*"], notFood: ["UBER-12","240508P10012", "CASHER", "印單:14", "品名", "數量", "金額", "消費額:", "810元", "結帳額:", "810元"])
+        let scanResult = self.createScanResult(foodReply: newFoodReply)
+        return scanResult
     }
     
     private func retryFiltering(_ detectedText: [String], completion: @escaping (ScanResult?) -> Void) {
