6ea3d146a72109624759c4545e2b3cd5789520ea
Yun Chieh Lee <melody0000tw@gmail.com>
2024-05-16 11:14:44 +0800

    delete demo fake datas

--- ReFridge/Managers/TextScanManager.swift
+++ ReFridge/Managers/TextScanManager.swift
@@ -30,24 +30,18 @@ class TextScanManager {
             try handler.perform(requests)
         } catch {
             print("Failed to perform request:", error)
-            let scanResult = self.createFakedatas()
-            completion(scanResult)
-//            completion(nil)
+            completion(nil)
         }
     }
     
     private func handleTextRecognitionResults(request: VNRequest, error: Error?, completion: @escaping (ScanResult?) -> Void) {
         if let error = error {
             print("Text detection error: \(error)")
-            let scanResult = self.createFakedatas()
-            completion(scanResult)
-//            completion(nil)
+            completion(nil)
             return
         }
         guard let observations = request.results as? [VNRecognizedTextObservation], !observations.isEmpty else {
-            let scanResult = self.createFakedatas()
-            completion(scanResult)
-//            completion(nil)
+            completion(nil)
             return
         }
 
@@ -58,35 +52,19 @@ class TextScanManager {
     private func processDetectedText(_ detectedText: [String], completion: @escaping (ScanResult?) -> Void) {
         openAIManager.filterArrays(inputArray: detectedText) { result in
             switch result {
-//            case .success(let foodReply) where foodReply.food.isEmpty:
-//                print("Food array is empty, retrying...")
-//                self.retryFiltering(detectedText, completion: completion)
+            case .success(let foodReply) where foodReply.food.isEmpty:
+                print("Food array is empty, retrying...")
+                self.retryFiltering(detectedText, completion: completion)
             case .success(let foodReply):
-                if foodReply.food.count < 3 || foodReply.notFood.count < 3 {
-                    print("less than 5")
-                    let scanResult = self.createFakedatas()
-                    completion(scanResult)
-                } else {
-                    print("more than 5")
-                    let scanResult = self.createScanResult(foodReply: foodReply)
-                    completion(scanResult)
-                }
+                let scanResult = self.createScanResult(foodReply: foodReply)
+                completion(scanResult)
             case .failure(let error):
                 print(error.localizedDescription)
                 print("error")
-//                let foodReply = AIFoodReplay(food: [], notFood: detectedText)
-//                let scanResult = self.createScanResult(foodReply: foodReply)
-                let scanResult = self.createFakedatas()
-                completion(scanResult)
+                let foodReply = AIFoodReplay(food: [], notFood: detectedText)
+                let scanResult = self.createScanResult(foodReply: foodReply)
             }
         }
-    }
-    
-    // create fake datas
-    private func createFakedatas() -> ScanResult {
-        let newFoodReply = AIFoodReplay(food: ["8吋墨西哥薄餅皮", "農心辛拉麵-黑", "二合一白咖啡80CT", "貓倍麗貓罐24入", "台灣雞清胸肉真空", "空運鮭魚切片"], notFood: ["COSTCO", "EWHOLESALE", "汐止店 874", "新北市 汐止區 221", "大同路一段158號", "統一編號", "16846757", "SALE", "金星會員 89377878301", "商品數小計 =", "295", "349", "599", "659", "546", "824", "295 T", "349 T", "599 T", "659 T", "546", "824", "總金額（T）", "現金", "找零", "（T-含稅）", "3.272", "3.302", "30", "05A2024 12:01 874 11 116 1032"])
-        let scanResult = self.createScanResult(foodReply: newFoodReply)
-        return scanResult
     }
     
     private func retryFiltering(_ detectedText: [String], completion: @escaping (ScanResult?) -> Void) {
